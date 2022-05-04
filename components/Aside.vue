ed521d342a63f5e0c1f653cd9f396df8bcda0946
omonovotabek <omonovotabek9541537@gmail.com>
2022-05-04 16:29:03 +0500

    as

--- components/Aside.vue
+++ components/Aside.vue
@@ -2,11 +2,8 @@
   <b-navbar>
       <b-navbar-nav class="d-block" style="height:100vh">
         <b-nav-item to="/">Обзор</b-nav-item>
-        <b-nav-item to="/analitc">Аналитика</b-nav-item>
-        <b-nav-item to="/assortment">Ассортимент</b-nav-item>
         <b-nav-item to="/history">История</b-nav-item>
-        <b-nav-item to="/addOrder">Добавить заказ</b-nav-item>
         <b-nav-item to="/logout">Выйти</b-nav-item>
       </b-navbar-nav>     
-    </b-navbar>  
+    </b-navbar>   
 </template>
